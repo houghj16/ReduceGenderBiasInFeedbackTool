cf10f8bf676bcbed5636b3ba7c0b19438bc6fdbb
Jessie Houghton <houghj@umich.edu>
2022-09-19 14:28:51 -0700

    Made progress on processing data from the frontend

--- ReduceGenderBiasInFeedbackTool/wwwroot/js/site.js
+++ ReduceGenderBiasInFeedbackTool/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function getAnalysis(userInput) {
+    return fetch(`Index?handler=AnalyzeFeedback&text=${userInput}`)
+        .then((response) => {
+            return response.text();
+        })
+}
+
+function updateMarker(sentiment) {
+    $("#markerValue").text(sentiment);
+}
+
+function updateAnalysis() {
+
+    var userInput = $("#Message").val();
+
+    getAnalysis(userInput)
+        .then((sentiment) => {
+            updateMarker(sentiment);
+        });
+}
+
+$("#Message").on('change input paste', updateAnalysis)
