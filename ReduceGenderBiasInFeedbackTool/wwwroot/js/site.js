111cf4bc62a35406275d0f322931d74deb47cbfd
Linda Zhao <lindazhao@microsoft.com>
2022-09-20 12:47:08 -0700

    adding modal for submission

--- ReduceGenderBiasInFeedbackTool/wwwroot/js/site.js
+++ ReduceGenderBiasInFeedbackTool/wwwroot/js/site.js
@@ -2,6 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var isBiased = false;
 function getAnalysis(userInput) {
     return fetch(`Index?handler=AnalyzeFeedback&text=${userInput}`)
         .then((response) => {
@@ -10,26 +11,62 @@ function getAnalysis(userInput) {
 }
 
 function updateMarker1(sentiment) {
+    if (sentiment == "We detected no biased words in your feedback! Great Job!") {
+        isBiased = false;
+    }
+    else {
+        isBiased = true;
+    }
     $("#markerValue1").text(sentiment);
 }
 
 function updateMarker2(sentiment) {
+    if (sentiment == "We detected no biased words in your feedback! Great Job!") {
+        isBiased = false;
+    }
+    else {
+        isBiased = true;
+    }
     $("#markerValue2").text(sentiment);
 }
 
 function updateMarker3(sentiment) {
+    if (sentiment == "We detected no biased words in your feedback! Great Job!") {
+        isBiased = false;
+    }
+    else {
+        isBiased = true;
+    }
     $("#markerValue3").text(sentiment);
 }
 
 function updateMarker4(sentiment) {
+    if (sentiment == "We detected no biased words in your feedback! Great Job!") {
+        isBiased = false;
+    }
+    else {
+        isBiased = true;
+    }
     $("#markerValue4").text(sentiment);
 }
 
 function updateMarker5(sentiment) {
+    if (sentiment == "We detected no biased words in your feedback! Great Job!") {
+        isBiased = false;
+    }
+    else {
+        isBiased = true;
+    }
     $("#markerValue5").text(sentiment);
 }
 
 function updateMarker6(sentiment) {
+    if (sentiment == "We detected no biased words in your feedback! Great Job!") {
+        isBiased = false;
+    }
+    else {
+        isBiased = true;
+    }
     $("#markerValue6").text(sentiment);
 }
 
@@ -94,12 +131,48 @@ function updateAnalysis6() {
         });
 }
 
+function showModal() {
+    console.log("print");
+    console.log($("myModal"));
+    $(".modal").css("display", "block");
+}
+
+function closeModal() {
+    $(".modal").css("display", "none");
+}
+
 $("#bias-check1").on('click', updateAnalysis1)
 $("#bias-check2").on('click', updateAnalysis2)
 $("#bias-check3").on('click', updateAnalysis3)
 $("#bias-check4").on('click', updateAnalysis4)
 $("#bias-check5").on('click', updateAnalysis5)
 $("#bias-check6").on('click', updateAnalysis6)
+
+var modal = document.getElementById("dialog");
+
+// Get the button that opens the modal
+var btn = document.getElementById("submit-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    if (isBiased) {
+        modal.style.display = "block";
+    }
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 //$("#Message1").on('change input paste', updateAnalysis1)
 //$("#Message2").on('change input paste', updateAnalysis2)
 //$("#Message3").on('change input paste', updateAnalysis3)
