fa2bcd109042b65c967ae4655994c0ecebc97d81
Kelly Dickson <kellydickson@microsoft.com>
2022-09-23 10:23:05 -0700

    Added code to call API in case we get an API key

---
+++ ReduceGenderBiasInFeedbackTool/Models/CallAPI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ConsoleProgram
+{
+    public class DataObject
+    {
+        public string Name { get; set; }
+    }
+
+    public class APICaller
+    {
+        private const string URL = "https://research.mondeca.com/demo/vago/";
+        private string urlParameters = "?api_key=123"; // Change when API key and params given
+
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+
+            APICaller vaguessnessAPI = new APICaller();
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response.
+            HttpResponseMessage response = client.GetAsync(vaguessnessAPI.urlParameters).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body.
+                var dataObjects = response.Content.ReadAsAsync<IEnumerable<DataObject>>().Result;  //Make sure to add a reference to System.Net.Http.Formatting.dll
+                foreach (var d in dataObjects)
+                {
+                    Console.WriteLine("{0}", d.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            // Make any other calls using HttpClient here.
+
+            // Dispose once all HttpClient calls are complete. This is not necessary if the containing object will be disposed of; for example in this case the HttpClient instance will be disposed automatically when the application terminates so the following call is superfluous.
+            client.Dispose();
+        }
+    }
+}
